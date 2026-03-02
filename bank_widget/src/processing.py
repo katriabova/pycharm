1830c03bc84d58e9be04fa22aa9fb11d8cfe3a40
Екатерина Рябова <katriabova@gmail.com>
2026-03-03 01:04:38 +0300

    final fix for pull request

--- bank_widget/src/processing.py
+++ bank_widget/src/processing.py
@@ -5,3 +5,4 @@ def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list
 
 def sort_by_date(data: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     return sorted(data, key=lambda x: x['date'], reverse=reverse)
+
