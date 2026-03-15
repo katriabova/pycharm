7530695b02664f09fc3e4abd9df46fb7fdb7311a
Екатерина Рябова <katriabova@gmail.com>
2026-03-16 00:23:32 +0300

    Fix: PEP8, README and structure

---
+++ bank_widget/src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    if not card_number:
+        return ""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    if not account_number:
+        return ""
+    return f"**{account_number[-4:]}"
