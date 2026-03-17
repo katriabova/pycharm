ef53c2891542dbed5f70c7887b48fecd6d76b575
Екатерина Рябова <katriabova@gmail.com>
2026-03-17 21:19:25 +0300

    Fix: structure, PEP8, pytest.ini and README for homework 10.2

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
+
 @pytest.mark.parametrize("card, expected", [
     ("7000792289606361", "7000 79** **** 6361"),
     ("1234567812345678", "1234 56** **** 5678"),
@@ -9,9 +10,10 @@ from src.masks import get_mask_card_number, get_mask_account
 def test_get_mask_card_number(card, expected):
     assert get_mask_card_number(card) == expected
 
+
 @pytest.mark.parametrize("account, expected", [
     ("73654108430135874305", "**4305"),
-    ("12345", "**2345"), # Короткий номер
+    ("12345", "**2345"),  # Короткий номер
     ("", "")
 ])
 def test_get_mask_account(account, expected):
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,14 +1,16 @@
 import pytest
 from src.widget import mask_account_card, get_date
 
+
 @pytest.mark.parametrize("input_str, expected", [
     ("Visa Gold 7000792289606361", "Visa Gold 7000 79** **** 6361"),
     ("Счет 73654108430135874305", "Счет **4305"),
     ("Maestro 1596837493215786", "Maestro 1596 83** **** 5786"),
-    ("", "Некорректные данные") # Пример обработки ошибок
+    ("", "Некорректные данные")  # Пример обработки ошибок
 ])
 def test_mask_account_card(input_str, expected):
     assert mask_account_card(input_str) == expected
+
 
 @pytest.mark.parametrize("date_str, expected", [
     ("2024-03-11T02:26:18.671407", "11.03.2024"),
